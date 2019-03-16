0b643687e86ecb0aae2ff5439ed59d553fb9686a
henz <henz.jb@gmail.com>
2019-03-16 15:04:20 +0900

    add utility class

---
+++ src/com/Utils.java
@@ -0,0 +1,31 @@
+package com;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Public utility class
+ */
+public class Utils {
+
+    /**
+     * Generate random numbers for a given amount and upper limit
+     * @param amountOfRandomNumber amount of random number to be generated
+     * @param upperLimit upper limit as the max value of the random number
+     * @return list of random random numbers
+     */
+    public static List generateRandomNumbers(int amountOfRandomNumber, int upperLimit){
+        List randomNumbers = new ArrayList();
+        Random rand = new Random();
+
+        for(int j=0; j<amountOfRandomNumber; j++){
+            int randomNumber = rand.nextInt(upperLimit);
+            randomNumbers.add(randomNumber);
+        }
+
+        return randomNumbers;
+    }
+
+}
+
