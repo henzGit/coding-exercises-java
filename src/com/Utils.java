c017c59c410834053b47e2e6d125932990ca2db4
henz <henz.jb@gmail.com>
2019-03-17 15:56:33 +0900

    add a new function to generate random floats

--- src/com/Utils.java
+++ src/com/Utils.java
@@ -11,20 +11,38 @@ public class Utils {
 
     /**
      * Generate random integers for a given amount and upper limit
-     * @param amountOfRandomNumber amount of random integer to be generated
+     * @param amountOfRandomInteger amount of random integer to be generated
      * @param upperLimit upper limit as the max value of the random integer
      * @return list of random integers
      */
-    public static List<Integer> generateRandomIntegers(int amountOfRandomNumber, int upperLimit){
-        List randomNumbers = new ArrayList();
+    public static List<Integer> generateRandomIntegers(int amountOfRandomInteger, int upperLimit){
+        List<Integer> randomIntegers = new ArrayList<>();
         Random rand = new Random();
 
-        for(int j=0; j<amountOfRandomNumber; j++){
-            int randomNumber = rand.nextInt(upperLimit);
-            randomNumbers.add(randomNumber);
+        for(int j = 0; j < amountOfRandomInteger; j++){
+            int randomInteger = rand.nextInt(upperLimit);
+            randomIntegers.add(randomInteger);
         }
 
-        return randomNumbers;
+        return randomIntegers;
+    }
+
+    /**
+     * Generate random floats for a given amount
+     * @param amountOfRandomFloats amount of random float to be generated
+     * @param multiplier multiplier for the generated float value
+     * @return list of random floats
+     */
+    public static List<Float> generateRandomFloats(int amountOfRandomFloats, int multiplier){
+        List<Float> randomFloats = new ArrayList<>();
+        Random rand = new Random();
+
+        for(int j = 0; j < amountOfRandomFloats; j++){
+            float randomFloat = multiplier * rand.nextFloat();
+            randomFloats.add(randomFloat);
+        }
+
+        return randomFloats;
     }
 
 }
