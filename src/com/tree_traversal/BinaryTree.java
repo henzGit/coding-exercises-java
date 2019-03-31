350217c4b532a5d173b8308a0faed233f456b125
henz <henz.jb@gmail.com>
2019-03-31 16:16:19 +0900

    add method to traverse binary tree

---
+++ src/com/tree_traversal/BinaryTree.java
@@ -0,0 +1,104 @@
+package com.tree_traversal;
+
+public class BinaryTree
+{
+    public static class Node
+    {
+        int key;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            key = item;
+            left = right = null;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+    }
+
+    // Root of Binary Tree
+    private Node root;
+
+    public BinaryTree()
+    {
+        root = null;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    /* Given a binary tree, print its nodes according to the
+        "bottom-up" postorder traversal. */
+    void printPostorder(Node node)
+    {
+        if (node == null)
+            return;
+
+        // first recur on left subtree
+        printPostorder(node.left);
+
+        // then recur on right subtree
+        printPostorder(node.right);
+
+        // now deal with the node
+        System.out.print(node.key + " ");
+    }
+
+    /* Given a binary tree, print its nodes in inorder*/
+    void printInorder(Node node)
+    {
+        if (node == null)
+            return;
+
+        /* first recur on left child */
+        printInorder(node.left);
+
+        /* then print the data of node */
+        System.out.print(node.key + " ");
+
+        /* now recur on right child */
+        printInorder(node.right);
+    }
+
+    /* Given a binary tree, print its nodes in preorder*/
+    void printPreorder(Node node)
+    {
+        if (node == null)
+            return;
+
+        /* first print data of node */
+        System.out.print(node.key + " ");
+
+        /* then recur on left sutree */
+        printPreorder(node.left);
+
+        /* now recur on right subtree */
+        printPreorder(node.right);
+    }
+
+    // Wrappers over above recursive functions
+    public void printPostorder() {	 printPostorder(root); }
+    public void printInorder()   {	 printInorder(root); }
+    public void printPreorder()  {	 printPreorder(root); }
+
+}
+
