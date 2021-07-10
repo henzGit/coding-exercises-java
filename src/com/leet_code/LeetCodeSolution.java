084693488c1ddf965790a96cd41978ea6542795f
Henz <henz.jb@gmail.com>
2021-07-10 19:21:44 +0900

    add LeetCodeSolution folder

---
+++ src/com/leet_code/LeetCodeSolution.java
@@ -0,0 +1,46 @@
+package com.leet_code;
+
+import java.util.PriorityQueue;
+
+/**
+ * Merge k Sorted Lists
+ * https://leetcode.com/problems/merge-k-sorted-lists/
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class LeetCodeSolution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (ListNode head: lists) {
+            while(head != null) {
+                minHeap.add(head.val);
+                head = head.next;
+            }
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        while(!minHeap.isEmpty()) {
+            head.next = new ListNode(minHeap.remove());
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
