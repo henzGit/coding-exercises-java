281570ea747a826520330158db317e8cf6e9b1db
Henz <henz.jb@gmail.com>
2021-07-19 20:40:42 +0900

    PalindromeLinkedList

---
+++ src/com/leet_code/PalindromeLinkedList.java
@@ -0,0 +1,37 @@
+package com.leet_code;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null) {
+            if (slow.next != null) slow = slow.next;
+            if (fast.next != null) fast = fast.next;
+            fast = fast.next;
+        }
+        ListNode prev = null;
+        while(slow != null) {
+            ListNode tmpNext = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = tmpNext;
+        }
+
+        ListNode n1 = head;
+        ListNode n2 = prev;
+        while(n1 != null && n2!= null) {
+            if(n1.val != n2.val) return false;
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return true;
+    }
+}
