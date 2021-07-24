b11f54a475514e13770253347dd5e69872f1f2ee
Henz <henz.jb@gmail.com>
2021-07-24 15:58:42 +0900

    upadate MeetingRoomsII

--- src/com/leet_code/MeetingRoomsII.java
+++ src/com/leet_code/MeetingRoomsII.java
@@ -7,7 +7,7 @@ public class MeetingRoomsII {
     public class Interval {
         int start;
         int end;
-        Interval() { start = 0; end = 0 }
+        Interval() { start = 0; end = 0; }
         Interval(int start, int end) {
             this.start = start;
             this.end = end;
