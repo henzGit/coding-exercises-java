ffb371f181984274cbf91c471abcdc7335a2cfc9
Henz <henz.jb@gmail.com>
2021-08-02 16:17:00 +0900

    add ValidPalindromeII

---
+++ src/com/leet_code/ValidPalindromeII.java
@@ -0,0 +1,39 @@
+package com.leet_code;
+
+/**
+ * Given a string s, return true if the s can be palindrome after deleting
+ * at most one character from it.
+ * https://leetcode.com/problems/valid-palindrome-ii/submissions/
+ */
+public class ValidPalindromeII {
+    public boolean validPalindrome(String s) {
+        return isPalin(s);
+    }
+
+    private boolean isPalin(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        char[] chars = s.toCharArray();
+        boolean alreadyDelete = false;
+        while(i < j) {
+            if(chars[i] != chars[j]) {
+                if(alreadyDelete) {
+                    return false;
+                }
+                if(chars[i] == chars[j-1] && chars[i+1] != chars[j]) {
+                    alreadyDelete = true;
+                    j--;
+                } else if (chars[i+1] == chars[j] && chars[i] != chars[j-1]) {
+                    alreadyDelete = true;
+                    i++;
+                } else if ( chars[i+1] != chars[j] && chars[i] != chars[j-1]) {
+                    return false;
+                }
+            }
+            i++;
+            j--;
+        }
+        return true;
+
+    }
+}
