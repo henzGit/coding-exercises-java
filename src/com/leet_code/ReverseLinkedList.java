e7f56b8317d02d41cceda8b88fa89aa253c7e233
Henz <henz.jb@gmail.com>
2021-08-15 20:38:05 +0900

    add ReverseLinkedList

---
+++ src/com/leet_code/ReverseLinkedList.java
@@ -0,0 +1,31 @@
+package com.leet_code;
+
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+        ListNode p = head;
+        ListNode prev = null;
+        while(p.next != null) {
+            ListNode tmp = p.next;
+            p.next = prev;
+            prev = p;
+            p = tmp;
+
+        }
+        p.next = prev;
+        return p;
+
+    }
+}
