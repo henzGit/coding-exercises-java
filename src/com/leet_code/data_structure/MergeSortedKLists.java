d725381ee2553d09069522d4866f8aa19b9073de
Henz <henz.jb@gmail.com>
2021-08-15 20:37:46 +0900

    remove unnecessary comment

--- src/com/leet_code/data_structure/MergeSortedKLists.java
+++ src/com/leet_code/data_structure/MergeSortedKLists.java
@@ -5,14 +5,6 @@ import java.util.PriorityQueue;
 /**
  * Merge k Sorted Lists
  * https://leetcode.com/problems/merge-k-sorted-lists/
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
  */
 
 public class MergeSortedKLists {
@@ -23,7 +15,6 @@ public class MergeSortedKLists {
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
-
 
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
