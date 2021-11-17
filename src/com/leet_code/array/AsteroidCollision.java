c773a8e983d247c9e2ae917c69ad8d8b147db1f6
Henz <henz.jb@gmail.com>
2021-11-17 21:56:16 +0900

    add AsteroidCollision

--- src/com/leet_code/array/AsteroidCollision.java
+++ src/com/leet_code/array/AsteroidCollision.java
@@ -17,23 +17,23 @@ import java.util.*;
 public class AsteroidCollision {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
-        for(int asteroid: asteroids) {
-            if(stack.isEmpty()) stack.push(asteroid);
+        for (int asteroid: asteroids) {
+            if (stack.isEmpty()) stack.push(asteroid);
             else {
-                if(stack.peek() < 0 && asteroid < 0) stack.push(asteroid);
-                else if(stack.peek() < 0 && asteroid > 0) stack.push(asteroid);
+                if (stack.peek() < 0 && asteroid < 0) stack.push(asteroid);
+                else if (stack.peek() < 0 && asteroid > 0) stack.push(asteroid);
                 else if (stack.peek() > 0 && asteroid > 0) stack.push(asteroid);
-                else if(stack.peek() > 0 && asteroid < 0) {
-                    if(stack.peek() == Math.abs(asteroid)) stack.pop();
-                    else if(stack.peek() > Math.abs(asteroid)) continue;
+                else if (stack.peek() > 0 && asteroid < 0) {
+                    if (stack.peek() == Math.abs(asteroid)) stack.pop();
+                    else if (stack.peek() > Math.abs(asteroid)) continue;
                     else {
-                        while(!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroid)) {
+                        while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroid)) {
                             stack.pop();
                         }
-                        if(stack.isEmpty()) stack.push(asteroid);
-                        else if(stack.peek() < 0) stack.push(asteroid);
-                        else if(stack.peek() > 0) {
-                            if(stack.peek() == Math.abs(asteroid)) stack.pop();
+                        if (stack.isEmpty()) stack.push(asteroid);
+                        else if (stack.peek() < 0) stack.push(asteroid);
+                        else if (stack.peek() > 0) {
+                            if (stack.peek() == Math.abs(asteroid)) stack.pop();
                         }
                     }
                 }
@@ -41,12 +41,9 @@ public class AsteroidCollision {
         }
 
         int[] out = new int[stack.size()];
-        for(int i = stack.size() - 1; i >= 0; i--) {
+        for (int i = stack.size() - 1; i >= 0; i--) {
             out[i] = stack.pop();
         }
-
         return out;
-
     }
-}
 }
