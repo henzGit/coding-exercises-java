b1c788764041b6248c76070ee0b5c850821f6180
Henz <henz.jb@gmail.com>
2022-02-28 16:15:57 +0900

    rename variable in LFUCache

--- src/com/leet_code/data_structure/LFUCache.java
+++ src/com/leet_code/data_structure/LFUCache.java
@@ -26,7 +26,7 @@ class LFUCache {
 
     HashMap<Integer, Integer> vals;//cache K and V
     HashMap<Integer, Integer> counts;//K and counters
-    HashMap<Integer, LinkedHashSet<Integer>> lists;//Counter and item list
+    HashMap<Integer, LinkedHashSet<Integer>> freqs;//Counter and item list
     int cap;
     int min = -1;
 
@@ -34,8 +34,8 @@ class LFUCache {
         cap = capacity;
         vals = new HashMap<>();
         counts = new HashMap<>();
-        lists = new HashMap<>();
-        lists.put(1, new LinkedHashSet<>());
+        freqs = new HashMap<>();
+        freqs.put(1, new LinkedHashSet<>());
     }
 
     public int get(int key) {
@@ -50,12 +50,12 @@ class LFUCache {
         // increase the counter
         counts.put(key, count + 1);
         // remove the element from the counter to linkedhashset
-        lists.get(count).remove(key);
+        freqs.get(count).remove(key);
 
         // when current min does not have any data, next one would be the min
-        if (count == min && lists.get(count).size() == 0) min++;
-        if (!lists.containsKey(count + 1)) lists.put(count + 1, new LinkedHashSet<>());
-        lists.get(count + 1).add(key);
+        if (count == min && freqs.get(count).size() == 0) min++;
+        if (!freqs.containsKey(count + 1)) freqs.put(count + 1, new LinkedHashSet<>());
+        freqs.get(count + 1).add(key);
     }
 
     public void put(int key, int value) {
@@ -67,8 +67,8 @@ class LFUCache {
             return;
         }
         if (vals.size() >= cap) {
-            int evit = lists.get(min).iterator().next();
-            lists.get(min).remove(evit);
+            int evit = freqs.get(min).iterator().next();
+            freqs.get(min).remove(evit);
             vals.remove(evit);
             counts.remove(evit);
         }
@@ -76,7 +76,7 @@ class LFUCache {
         vals.put(key, value);
         counts.put(key, 1);
         min = 1;
-        lists.get(1).add(key);
+        freqs.get(1).add(key);
     }
 
 }
