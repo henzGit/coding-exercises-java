cb0a9efcaf45b2b55ad004bc93549024671f239e
Henz <henz.jb@gmail.com>
2022-04-17 06:57:37 +0900

    nt LowestCommonAncestorOfABinaryTree

--- src/com/leet_code/tree/LowestCommonAncestorOfABinaryTree.java
+++ src/com/leet_code/tree/LowestCommonAncestorOfABinaryTree.java
@@ -23,9 +23,9 @@ public class LowestCommonAncestorOfABinaryTree {
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null)return null;
-        if(p.val<root.val && q.val<root.val) return lowestCommonAncestor(root.left,p,q);
-        if(p.val>root.val && q.val>root.val) return lowestCommonAncestor(root.right,p,q);
+        if (root == null) return null;
+        if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+        if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
         return root;
     }
 }
